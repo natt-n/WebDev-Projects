f558c6517b7e84d99db2031aad6144a2d06e3020
Natasha Naorem <natasha.naorem@gmail.com>
2025-01-02 01:33:34 -0500

    update

--- Lessons/DOM lessons/DOM Challenge Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Nat";
-
-document.querySelector("li a").style.color = "red";
-
-document.querySelector("button").style.backgroundColor = "yellow";
-
-document.querySelector("h1").classList.add("huge");
--- Lessons/jQuery/lesson1.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    
-})
-$("h1"); //document.querySelector("h1");
