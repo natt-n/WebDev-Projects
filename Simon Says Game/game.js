836ba748b15ea817df0bb92fafbbbd9e33237491
Natasha Naorem <natasha.naorem@gmail.com>
2025-03-19 17:30:34 -0400

    handles gameover reset and key press to start

--- Simon Says Game/game.js
+++ Simon Says Game/game.js
@@ -1,6 +1,7 @@
 gamePattern = [];
 buttonColours = ["red", "blue", "green", "yellow"];
 userClickedPattern = [];
+var level = 1;
 
 function nextSequence(){ //choose a random number between 0 and 3
     var randomNumber = Math.floor(Math.random() * 4)
@@ -28,7 +29,17 @@ function animatePress(currentColour){
     }, 100);
 }
 
+function checkAnswer(){
+    for (var i = 0; i < userClickedPattern.length; i++){
+        if(userClickedPattern[i] !== gamePattern[i]){
+            return false;
+        }
+    }
+    return true;
+}
+//start the game with keypress
 $(document).on("keypress", function(){
+    $(".title").text("Level " + level);
     var randomChosenColour = nextSequence();
     gamePattern.push(buttonColours[randomChosenColour]); //add the new colour to the gamePattern array
     console.log(gamePattern);
@@ -36,59 +47,22 @@ $(document).on("keypress", function(){
 }
 );
 
+//will always check if the buttons pressed, matches the gamePattern
 $(".btn").on("click", function(){
     var userChosenColour = $(this).attr("id");
-    console.log("Pressed: " + userChosenColour);
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
-});
 
-
-
-/*
-var gameRunning = false;
-var getUserKeys = false;
-
-$(document).on("keypress", function(){
-    gameRunning = true;
-
-    while(gameRunning){
-        console.log("Game is running");
-        
-        pressedKeys = 0;
-        userCorrect = false;
-
-        if(pressedKeys == 0 || userCorrect){
-            var randomChosenColour = newSequence();
-            gamePattern.push(buttonColours[randomChosenColour]); //add the new colour to the gamePattern array
-            playSequence(gamePattern); //play the sequence of colours
-        }
-        
-        while(pressedKeys < gamePattern.length){
-            console.log("User can click");
-
-            $(document).on("click", function(){
-                var userChosenColour = $(this).attr("id");
-                console.log("Pressed: " + userChosenColour);
-
-                if(userChosenColour !== gamePattern[pressedKeys]){
-                    console.log("User has clicked the wrong colour");
-                    userCorrect = false;
-                    pressedKeys = gamePattern.length;
-                    gameRunning = false;
-                } else{
-                    userCorrect = true;
-                }
-            });
-            
-            pressedKeys++;
-        }
-    }
-
-    if(!gameRunning){
-        console.log("Game ended");
+    if(checkAnswer()){
+        console.log("Correct");
+        userClickedPattern = [];
+        level++;
+    } else{
+        console.log("Wrong");
+        userClickedPattern = [];
+        gamePattern = [];
+        level = 1;
     }
 });
-*/
