4200f3cc0b667544008a4825b462ddf9aef26f2e
Natasha Naorem <natasha.naorem@gmail.com>
2025-03-19 17:49:03 -0400

    completed game

--- Simon Says Game/game.js
+++ Simon Says Game/game.js
@@ -17,6 +17,11 @@ function playSequence(gamePattern){ //plays the sequence of colours
     }
 }
 
+function play(color){
+    playSound(color);
+    $("#" + color).fadeOut(100).fadeIn(100);
+}
+
 function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
@@ -37,30 +42,55 @@ function checkAnswer(){
     }
     return true;
 }
-//start the game with keypress
-$(document).on("keypress", function(){
-    $(".title").text("Level " + level);
+
+function nextLevel(){
+    $("h1").text("Level " + level);
     var randomChosenColour = nextSequence();
     gamePattern.push(buttonColours[randomChosenColour]); //add the new colour to the gamePattern array
-    console.log(gamePattern);
-    playSequence(gamePattern); //play the sequence of colours
+    console.log("Game Pattern: " + gamePattern);
+    play(buttonColours[randomChosenColour]); //play the new colour
+}
+
+//start the game with keypress
+$(document).on("keypress", function(){
+    if(gamePattern.length === 0){
+        nextLevel();
+    } else{ //ignore otherwise
+        return;
+    }
 }
 );
 
 //will always check if the buttons pressed, matches the gamePattern
 $(".btn").on("click", function(){
+
+    //user clicks button
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    console.log("Clicked: " + userClickedPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
 
     if(checkAnswer()){
         console.log("Correct");
-        userClickedPattern = [];
-        level++;
+        setTimeout(function(){
+            userClickedPattern = [];
+            level++;
+            nextLevel();
+        }, 1000);
+
+        if(userClickedPattern.length !== gamePattern.length){
+            return;
+        }
+
     } else{
         console.log("Wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
         userClickedPattern = [];
         gamePattern = [];
         level = 1;
